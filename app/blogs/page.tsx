66897e596ad477587f73a18faa821082b1874c39
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2026-02-22 00:13:12 +0330

    blogs too

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -29,7 +29,7 @@ export default async function BlogPage() {
       <div className="pointer-events-none absolute inset-0 bg-black/65" />
       <div className="pointer-events-none absolute inset-0 bg-linear-to-b from-black/10 via-transparent to-black/30" />
 
-      <section className="relative mx-auto max-w-6xl px-5 sm:px-6 md:px-8 py-20 sm:py-24 md:py-28 space-y-20 bg-gray-900/55 backdrop-blur-md rounded-xl border border-gray-800/40">
+      <section className="relative mx-auto max-w-4xl px-5 sm:px-6 md:px-7 pt-12 sm:pt-16 md:pt-20 pb-5 sm:pb-6 md:pb-7 space-y-12 sm:space-y-16 bg-gray-900/55 backdrop-blur-md rounded-xl border border-gray-800/40">
 
         <header className="space-y-6 max-w-3xl">
           <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold tracking-tight text-stone-50">
